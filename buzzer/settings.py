85b4d3f5d2bef9f419cd2f7aa4069bacbff30eb9
tanushree <tanu283shree@gmail.com>
2021-04-15 14:38:23 +0530

    Settings updated

--- buzzer/settings.py
+++ buzzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--iaw3x_8egke@953lrjaoebm%!$ayim603e6dd=l6nagsnycn1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['awakinn.xyz']
 
 
 # Application definition
